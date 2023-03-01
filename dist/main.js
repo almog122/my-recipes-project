04ee8d3430100412a8ecb7e8d7f1c2ccba9a34f1
Almog <almo321@gmail.com>
2023-03-01 18:45:16 +0200

    show recpies and click photo alert

--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,17 @@
-const renderer = new Renderer()
-
-const fetch = function () {
-  $.get("/wonders", function (response) {
-    renderer.renderPage(response);
-  });
-};
+const renderer = new Renderer();
 
 const getRecpiesByIngredient = function () {
   let ingredient = $("#ingredient-input").val();
   $.get(`/recipes/${ingredient}`)
-  .then((response) => {
-    renderer.renderPage(response);
-  })
-  .catch((error) => console.log(`No recipes with ${ingredient} found`));
-  
+    .then((response) => {
+      renderer.renderPage(response);
+    })
+    .catch((error) => console.log(`No recipes with ${ingredient} found`));
 };
+
+const firstIngredientAlert = function () {
+  let ingredient = $(this).closest(".recipe").find(".Ingredients").children()[0].innerHTML;
+  alert(ingredient);
+};
+
+$("body").on("click", "img", firstIngredientAlert);
